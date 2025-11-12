4dd6b9ab58d5739d1569755aecff5130e66aa5ae
yarty777 <yaroslav0908l@gmail.com>
2025-11-12 09:11:00 +0200

    Added elements of tje form

--- LW3_OKR/LW3_OKR/Form1.cs
+++ LW3_OKR/LW3_OKR/Form1.cs
@@ -1,10 +1,12 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
 namespace LW3_OKR
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+
         }
     }
 }
