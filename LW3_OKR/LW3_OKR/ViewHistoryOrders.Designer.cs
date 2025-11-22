0548ad87177811f2102df25d0672ad7a4e534574
EzX_Anub1s <ivantop625638@gmail.com>
2025-11-22 17:38:24 +0200

    Відредагував форму по стилю

--- LW3_OKR/LW3_OKR/ViewHistoryOrders.Designer.cs
+++ LW3_OKR/LW3_OKR/ViewHistoryOrders.Designer.cs
@@ -40,9 +40,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = Color.Cyan;
+            label1.BackColor = Color.White;
             label1.Font = new Font("Showcard Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(475, 153);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(365, 153);
             label1.Name = "label1";
             label1.Size = new Size(226, 21);
             label1.TabIndex = 0;
@@ -52,7 +53,7 @@
             // 
             textBox1.BackColor = Color.White;
             textBox1.ForeColor = Color.Red;
-            textBox1.Location = new Point(476, 186);
+            textBox1.Location = new Point(366, 186);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(122, 27);
             textBox1.TabIndex = 1;
@@ -61,7 +62,7 @@
             // 
             textBox2.BackColor = Color.White;
             textBox2.ForeColor = Color.Red;
-            textBox2.Location = new Point(475, 89);
+            textBox2.Location = new Point(365, 89);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(153, 27);
             textBox2.TabIndex = 2;
@@ -69,10 +70,10 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.BackColor = Color.Cyan;
+            label2.BackColor = Color.White;
             label2.Font = new Font("Bauhaus 93", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.InactiveCaptionText;
-            label2.Location = new Point(475, 63);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(365, 63);
             label2.Name = "label2";
             label2.Size = new Size(275, 23);
             label2.TabIndex = 3;
@@ -81,10 +82,10 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.BackColor = Color.FromArgb(0, 0, 192);
+            label3.BackColor = Color.White;
             label3.Font = new Font("Vladimir Script", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.Yellow;
-            label3.Location = new Point(12, 20);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(6, 22);
             label3.Name = "label3";
             label3.Size = new Size(209, 28);
             label3.TabIndex = 5;
@@ -94,14 +95,15 @@
             // 
             listView1.Location = new Point(6, 63);
             listView1.Name = "listView1";
-            listView1.Size = new Size(463, 330);
+            listView1.Size = new Size(340, 330);
             listView1.TabIndex = 6;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
             // button1
             // 
-            button1.BackColor = Color.Cyan;
-            button1.Location = new Point(490, 321);
+            button1.BackColor = Color.White;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(352, 329);
             button1.Name = "button1";
             button1.Size = new Size(156, 51);
             button1.TabIndex = 7;
@@ -112,9 +114,8 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.Control;
-            BackgroundImage = Properties.Resources.FonHistory;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Cornsilk;
+            ClientSize = new Size(653, 450);
             Controls.Add(button1);
             Controls.Add(listView1);
             Controls.Add(label3);
