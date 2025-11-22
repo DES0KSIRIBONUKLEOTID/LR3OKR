978ab3e2ae784111b670105aa510eccf12aed144
baranov <danyab554@gmail.com>
2025-11-22 12:36:52 +0200

    Функціонал для історії замовлень

--- LW3_OKR/LW3_OKR/ViewHistoryOrders.Designer.cs
+++ LW3_OKR/LW3_OKR/ViewHistoryOrders.Designer.cs
@@ -29,52 +29,56 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            textBoxSum = new TextBox();
+            textBoxDate = new TextBox();
             label2 = new Label();
             label3 = new Label();
             listView1 = new ListView();
-            button1 = new Button();
+            buttonFind = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Cyan;
-            label1.Font = new Font("Showcard Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(475, 153);
+            label1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(594, 191);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(226, 21);
+            label1.Size = new Size(273, 25);
             label1.TabIndex = 0;
             label1.Text = "Загальна сума замовлень";
             // 
-            // textBox1
+            // textBoxSum
             // 
-            textBox1.BackColor = Color.White;
-            textBox1.ForeColor = Color.Red;
-            textBox1.Location = new Point(476, 186);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(122, 27);
-            textBox1.TabIndex = 1;
+            textBoxSum.BackColor = Color.White;
+            textBoxSum.ForeColor = Color.Red;
+            textBoxSum.Location = new Point(595, 232);
+            textBoxSum.Margin = new Padding(4, 4, 4, 4);
+            textBoxSum.Name = "textBoxSum";
+            textBoxSum.Size = new Size(152, 31);
+            textBoxSum.TabIndex = 1;
             // 
-            // textBox2
+            // textBoxDate
             // 
-            textBox2.BackColor = Color.White;
-            textBox2.ForeColor = Color.Red;
-            textBox2.Location = new Point(475, 89);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(153, 27);
-            textBox2.TabIndex = 2;
+            textBoxDate.BackColor = Color.White;
+            textBoxDate.ForeColor = Color.Red;
+            textBoxDate.Location = new Point(594, 111);
+            textBoxDate.Margin = new Padding(4, 4, 4, 4);
+            textBoxDate.Name = "textBoxDate";
+            textBoxDate.Size = new Size(190, 31);
+            textBoxDate.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.Cyan;
-            label2.Font = new Font("Bauhaus 93", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.InactiveCaptionText;
-            label2.Location = new Point(475, 63);
+            label2.Location = new Point(594, 79);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(275, 23);
+            label2.Size = new Size(428, 29);
             label2.TabIndex = 3;
             label2.Text = "Введіть дату вашого замовлення";
             // 
@@ -82,46 +86,51 @@
             // 
             label3.AutoSize = true;
             label3.BackColor = Color.FromArgb(0, 0, 192);
-            label3.Font = new Font("Vladimir Script", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Yellow;
-            label3.Location = new Point(12, 20);
+            label3.Location = new Point(15, 25);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(209, 28);
+            label3.Size = new Size(270, 32);
             label3.TabIndex = 5;
             label3.Text = "Історія замовлень";
             // 
             // listView1
             // 
-            listView1.Location = new Point(6, 63);
+            listView1.Location = new Point(8, 79);
+            listView1.Margin = new Padding(4, 4, 4, 4);
             listView1.Name = "listView1";
-            listView1.Size = new Size(463, 330);
+            listView1.Size = new Size(578, 412);
             listView1.TabIndex = 6;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
-            // button1
+            // buttonFind
             // 
-            button1.BackColor = Color.Cyan;
-            button1.Location = new Point(490, 321);
-            button1.Name = "button1";
-            button1.Size = new Size(156, 51);
-            button1.TabIndex = 7;
-            button1.Text = "Знайти замовлення за датою";
-            button1.UseVisualStyleBackColor = false;
+            buttonFind.BackColor = Color.Cyan;
+            buttonFind.Location = new Point(612, 401);
+            buttonFind.Margin = new Padding(4, 4, 4, 4);
+            buttonFind.Name = "buttonFind";
+            buttonFind.Size = new Size(195, 64);
+            buttonFind.TabIndex = 7;
+            buttonFind.Text = "Знайти замовлення за датою";
+            buttonFind.UseVisualStyleBackColor = false;
+            buttonFind.Click += buttonFind_Click;
             // 
             // ViewHistoryOrders
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             BackgroundImage = Properties.Resources.FonHistory;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(buttonFind);
             Controls.Add(listView1);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(textBoxDate);
+            Controls.Add(textBoxSum);
             Controls.Add(label1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "ViewHistoryOrders";
             Text = "ViewHistoryOrders";
             ResumeLayout(false);
@@ -131,11 +140,11 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox textBoxSum;
+        private TextBox textBoxDate;
         private Label label2;
         private Label label3;
         private ListView listView1;
-        private Button button1;
+        private Button buttonFind;
     }
 }
