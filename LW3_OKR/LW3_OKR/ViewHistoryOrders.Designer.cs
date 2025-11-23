588cb1196693a0e92c6a3bd35f6079d59e27b667
yarty777 <yaroslav0908l@gmail.com>
2025-11-23 14:48:42 +0200

    fixed conflict

--- LW3_OKR/LW3_OKR/ViewHistoryOrders.Designer.cs
+++ LW3_OKR/LW3_OKR/ViewHistoryOrders.Designer.cs
@@ -35,6 +35,7 @@
             label3 = new Label();
             listView1 = new ListView();
             buttonFind = new Button();
+            find = new Button();
             SuspendLayout();
             // 
             // label1
@@ -43,29 +44,28 @@
             label1.BackColor = Color.White;
             label1.Font = new Font("Showcard Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Location = new Point(365, 153);
+            label1.Location = new Point(292, 122);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(273, 25);
+            label1.Size = new Size(226, 21);
             label1.TabIndex = 0;
             label1.Text = "Загальна сума замовлень";
             // 
             // textBoxSum
             // 
-            textBox1.BackColor = Color.White;
-            textBox1.ForeColor = Color.Red;
-            textBox1.Location = new Point(366, 186);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(122, 27);
-            textBox1.TabIndex = 1;
+            textBoxSum.Location = new Point(339, 156);
+            textBoxSum.Margin = new Padding(2);
+            textBoxSum.Name = "textBoxSum";
+            textBoxSum.Size = new Size(134, 27);
+            textBoxSum.TabIndex = 8;
             // 
             // textBoxDate
             // 
-            textBox2.BackColor = Color.White;
-            textBox2.ForeColor = Color.Red;
-            textBox2.Location = new Point(365, 89);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(153, 27);
-            textBox2.TabIndex = 2;
+            textBoxDate.Location = new Point(339, 84);
+            textBoxDate.Margin = new Padding(2);
+            textBoxDate.Name = "textBoxDate";
+            textBoxDate.Size = new Size(134, 27);
+            textBoxDate.TabIndex = 7;
             // 
             // label2
             // 
@@ -73,9 +73,10 @@
             label2.BackColor = Color.White;
             label2.Font = new Font("Bauhaus 93", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ActiveCaptionText;
-            label2.Location = new Point(365, 63);
+            label2.Location = new Point(292, 50);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(428, 29);
+            label2.Size = new Size(275, 23);
             label2.TabIndex = 3;
             label2.Text = "Введіть дату вашого замовлення";
             // 
@@ -85,18 +86,18 @@
             label3.BackColor = Color.White;
             label3.Font = new Font("Vladimir Script", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.ActiveCaptionText;
-            label3.Location = new Point(6, 22);
+            label3.Location = new Point(5, 18);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(270, 32);
+            label3.Size = new Size(209, 28);
             label3.TabIndex = 5;
             label3.Text = "Історія замовлень";
             // 
             // listView1
             // 
-            listView1.Location = new Point(8, 79);
-            listView1.Margin = new Padding(4, 4, 4, 4);
+            listView1.Location = new Point(6, 63);
             listView1.Name = "listView1";
-            listView1.Size = new Size(340, 330);
+            listView1.Size = new Size(273, 265);
             listView1.TabIndex = 6;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -104,7 +105,7 @@
             // 
             buttonFind.BackColor = Color.Cyan;
             buttonFind.Location = new Point(612, 401);
-            buttonFind.Margin = new Padding(4, 4, 4, 4);
+            buttonFind.Margin = new Padding(4);
             buttonFind.Name = "buttonFind";
             buttonFind.Size = new Size(195, 64);
             buttonFind.TabIndex = 7;
@@ -112,20 +113,29 @@
             buttonFind.UseVisualStyleBackColor = false;
             buttonFind.Click += buttonFind_Click;
             // 
+            // find
+            // 
+            find.Location = new Point(311, 294);
+            find.Name = "find";
+            find.Size = new Size(186, 34);
+            find.TabIndex = 9;
+            find.Text = "Найти";
+            find.UseVisualStyleBackColor = true;
+            find.Click += buttonFind_Click;
+            // 
             // ViewHistoryOrders
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Cornsilk;
-            ClientSize = new Size(653, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(522, 360);
+            Controls.Add(find);
             Controls.Add(listView1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(textBoxDate);
             Controls.Add(textBoxSum);
             Controls.Add(label1);
-            Margin = new Padding(4, 4, 4, 4);
             Name = "ViewHistoryOrders";
             Text = "ViewHistoryOrders";
             ResumeLayout(false);
@@ -141,5 +151,6 @@
         private Label label3;
         private ListView listView1;
         private Button buttonFind;
+        private Button find;
     }
 }
